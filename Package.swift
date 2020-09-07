7135e9c617d2239679ab83810793b5e93298c45e
Tuwuh S <tuwuhs@gmail.com>
2020-09-06 21:55:02 -0400

    Switch to my (tuwuhs) fork of SwiftFusion for development

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/borglab/SwiftFusion.git", .branch("master")),
+        .package(url: "https://github.com/tuwuhs/SwiftFusion.git", .branch("dev-handeye")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
