5af3915a4f588417f05686d340fcef005788cfc3
Tuwuh S <tuwuhs@gmail.com>
2020-11-17 15:01:07 -0500

    Switch back to SwiftFusion master branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
     // .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark.git", .branch("master")),
-    .package(url: "https://github.com/tuwuhs/SwiftFusion.git", .branch("dev-handeye")),
+    .package(url: "https://github.com/tuwuhs/SwiftFusion.git", .branch("master")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
